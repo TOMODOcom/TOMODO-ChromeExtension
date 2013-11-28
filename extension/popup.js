6c1c4643a3835fa7c2e3afa14952f0ca786fb29a
yonatan <yonatan.braude@tomodo.com>
2013-11-28 17:59:44 +0200

    fixed new tab open bug

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-    $('#tomodo a').click(function(){
+    $('#tomodo .modit').click(function(){
         chrome.tabs.getAllInWindow(function(tabs){
             var tab = tabs.filter(function(tab){return tab.active})[0];
             console.log('tab id is: ', tab.id);
