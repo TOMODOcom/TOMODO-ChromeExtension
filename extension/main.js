da46921687806dc2001ad31fd57ad6e3833afa80
yonatan <yonatan.braude@tomodo.com>
2013-11-28 20:17:40 +0200

    changed to comply with getPublishedModsByDomain

--- extension/main.js
+++ extension/main.js
@@ -4,15 +4,15 @@ function setNumberOfMod(){
         host = host.split('.');
         host = host.slice(-2);
         host = host.join('.');
-        mod_request = new XMLHttpRequest;
-        mod_request.open('post', 'http://betterinternethome.com:8080/v1/getModData', false);
+        var mods_request = new XMLHttpRequest;
+        mods_request.open('post', 'http://betterinternethome.com:8080/v1/getPublishedModsByDomain', false);
         var request_data = {target_domain:host}
         request_data = JSON.stringify(request_data);
-        mod_request.send(request_data);
-        var mod_data = mod_request.responseText;
-        mod_data = JSON.parse(mod_data);
-        mod_data = JSON.parse(mod_data);
-        var badgeText = mod_data.count > 0 ? mod_data.count.toLocaleString() : "";
+        mods_request.send(request_data);
+        var mods_data = mods_request.responseText;
+        mods_data = JSON.parse(mods_data);
+        var count = mods_data.length;
+        var badgeText = count > 0 ? count.toLocaleString() : "";
         localStorage.badgeText = badgeText;
     }
     chrome.runtime.sendMessage({badgeText : localStorage.badgeText, host: location.hostname });
