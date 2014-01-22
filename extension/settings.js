fcc44aa79b23fe4ddc6e23c98483b3ee8b8d1593
yonatan <yonatan.braude@tomodo.com>
2014-01-22 18:12:36 +0200

    one logger to rule them all!

--- extension/settings.js
+++ extension/settings.js
@@ -1,14 +1,12 @@
 // settings = {
-//      API_URL: "http://betterinternethome.com:8080",
-//      HOME_URL: "http://betterinternethome.com:8000"
+      // API_URL: "http://betterinternethome.com:8080",
+      // HOME_URL: "http://betterinternethome.com:8000"
 // }
-
-//settings = {
-//   API_URL: "http://api.tomodo.com",
-//   HOME_URL: "http://tomodo.com"
-//}
-
+// settings = {
+  // API_URL: "http://api.tomododev.com",
+  // HOME_URL: "http://tomododev.com"
+// }
 settings = {
-   API_URL: "http://api.tomododev.com",
-   HOME_URL: "http://tomododev.com"
+  API_URL: "http://api.tomodo.com",
+  HOME_URL: "http://tomodo.com"
 }
